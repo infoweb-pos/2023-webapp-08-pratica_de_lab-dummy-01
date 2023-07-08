5dd4d282854528f7aa3bde45a9a29d93c9bc9340
Leonardo Minora <leonardo.minora@gmail.com>
2023-07-08 09:18:46 -0300

    passo 4 - html no jsx de PaginaTarefas

--- src/paginas/PaginaTarefas.tsx
+++ src/paginas/PaginaTarefas.tsx
@@ -1,7 +1,24 @@
 const PaginaTarefas = () => {
+	const tarefa = {
+		id: 1,
+		todo: "codificar requisições a api dummy json",
+		completed: false,
+		userId: 1,
+	};
+
 	return (
 		<div className="card">
 			<h2>Lista de tarefas</h2>
+			<div className="card">
+				<label>
+					<input
+						type="checkbox"
+						name={`${tarefa.id}`}
+						checked={tarefa.completed}
+					/>
+					{tarefa.todo}
+				</label>
+			</div>
 		</div>
 	);
 };
