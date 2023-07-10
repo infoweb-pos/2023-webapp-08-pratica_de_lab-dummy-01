30aac7a24ccdd19be9acc7896e2dba9bd101030e
Leonardo Minora <leonardo.minora@gmail.com>
2023-07-10 07:36:55 -0300

    passo 5 - conectado estado ao html

--- src/paginas/PaginaTarefasNova.tsx
+++ src/paginas/PaginaTarefasNova.tsx
@@ -1,17 +1,27 @@
+import { useState } from "react";
+
 const PaginaTarefasNova = () => {
-    return (
-        <div className="card">
-            <h2>Lista de tarefas</h2>
+	const [tarefa, setTarefa] = useState("");
+
+	return (
+		<div className="card">
+			<h2>Lista de tarefas</h2>
 			<h3>Tarefa nova</h3>
-            <div>
-                <label>
-                    Tarefa:&nbsp;
-                    <input type="text"></input>
-                </label>
-                <button>Criar nova tarefa</button>
-            </div>
-        </div>
-    );
-}
+			<div>
+				<label>
+					Tarefa:&nbsp;
+					<input
+						type="text"
+						value={tarefa}
+						onChange={(evento: React.FormEvent<HTMLInputElement>) =>
+							setTarefa(evento.target.value)
+						}
+					></input>
+				</label>
+				<button>Criar nova tarefa</button>
+			</div>
+		</div>
+	);
+};
 
 export default PaginaTarefasNova;
