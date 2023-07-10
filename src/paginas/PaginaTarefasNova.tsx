0a55de600bb5a4b64c46d9699900fb835eb7cf6f
Leonardo Minora <leonardo.minora@gmail.com>
2023-07-10 07:01:25 -0300

    passo 2 - criado página de nova tarefa

---
+++ src/paginas/PaginaTarefasNova.tsx
@@ -0,0 +1,10 @@
+const PaginaTarefasNova = () => {
+    return (
+        <div className="card">
+            <h2>Lista de tarefas</h2>
+			<h3>Tarefa nova</h3>
+        </div>
+    );
+}
+
+export default PaginaTarefasNova;
