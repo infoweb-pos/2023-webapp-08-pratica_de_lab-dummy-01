39481378f700eca994faf123e6ae03d3dbaaee0c
Leonardo Minora <leonardo.minora@gmail.com>
2023-07-10 07:10:58 -0300

    passo 4 - criado html para entrada do texto da nova tarefa

--- src/paginas/PaginaTarefasNova.tsx
+++ src/paginas/PaginaTarefasNova.tsx
@@ -3,6 +3,13 @@ const PaginaTarefasNova = () => {
         <div className="card">
             <h2>Lista de tarefas</h2>
 			<h3>Tarefa nova</h3>
+            <div>
+                <label>
+                    Tarefa:&nbsp;
+                    <input type="text"></input>
+                </label>
+                <button>Criar nova tarefa</button>
+            </div>
         </div>
     );
 }
